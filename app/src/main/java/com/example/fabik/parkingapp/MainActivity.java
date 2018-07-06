2e3720905eb841569ae20996a676bc3b4f3a5a8e
Fabian Ardila - Portatil <fabiking@outlook.com>
2018-07-06 18:30:43 -0500

    Initial commit

---
+++ app/src/main/java/com/example/fabik/parkingapp/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.fabik.parkingapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText et1, et2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        et1 = findViewById(R.id.etPlaca);
+        et2 = findViewById(R.id.etHoraIngreso);
+
+
+
+
+    }
+
+    public void btnAtras (View view){
+        Intent intent = new Intent(this, MenuPrincipal.class);
+        startActivity(intent);
+    }
+
+
+}
