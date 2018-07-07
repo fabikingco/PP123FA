54e688f972ebb2a14b47801a5cb2eb0cf8d3ec3c
Fabian Ardila - Portatil <fabiking@outlook.com>
2018-07-07 12:00:58 -0500

    IMPLEMENTACION - Mejoras en la app
    Se creo el drawable de backgraund para poner la placa
    Se creo la compatibilidad con maquinas con android 5.0 con el drawable.

--- app/src/main/java/com/example/fabik/parkingapp/IngresarVehiculos.java
+++ app/src/main/java/com/example/fabik/parkingapp/IngresarVehiculos.java
@@ -2,8 +2,13 @@ package com.example.fabik.parkingapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class IngresarVehiculos extends AppCompatActivity {
+
+    TextView tv1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,5 +16,18 @@ public class IngresarVehiculos extends AppCompatActivity {
         setContentView(R.layout.activity_ingresar_vehiculos);
 
         setTitle("Ingresar Vehoculos");
+
+        tv1 = findViewById(R.id.tvHora);
+
+        Calendar myDate = Calendar.getInstance();
+        int hora = myDate.get( Calendar.HOUR);
+        int minute = myDate.get(Calendar.MINUTE);
+        int segundos = myDate.get(Calendar.SECOND);
+
+
+
+        tv1.setText(String.valueOf(segundos)+" "+String.valueOf(minute)+" "+String.valueOf(hora))
+        ;
+
     }
 }
