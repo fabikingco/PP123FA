d733bc4e40c451c86e8bd7258abfbea0c30e9138
Fabian Ardila <fabianardila@wposs.com>
2020-01-15 13:53:18 -0500

    Libreria de Printooth

--- app/src/main/java/com/example/fabik/parkingapp/appInit.java
+++ app/src/main/java/com/example/fabik/parkingapp/appInit.java
@@ -2,9 +2,12 @@ package com.example.fabik.parkingapp;
 
 import android.app.Application;
 import android.content.Context;
+import android.os.Build;
 
 import com.example.fabik.parkingapp.Printer.PrintManager;
 import com.example.fabik.parkingapp.Printer.utils.LogUtil;
+
+import com.mazenrashed.printooth.Printooth;
 import com.pos.device.SDKManager;
 import com.pos.device.SDKManagerCallback;
 
@@ -20,17 +23,24 @@ public class appInit extends Application {
     public void onCreate() {
         super.onCreate();
         init();
-    }
-    private void init(){
 
-        appContext = getApplicationContext();
-        SDKManager.init(appInit.appContext, new SDKManagerCallback() {
-            @Override
-            public void onFinish() {
-                LogUtil.e("init SDK success(threadname: "+Thread.currentThread().getName()+")");
-            }
-        });
-        PrintManager.init(getApplicationContext());
+    }
+
+    private void init() {
+        System.out.println("MODELO " + Build.MODEL);
+        if (Build.MODEL.equals("NEW9220")) {
+            appContext = getApplicationContext();
+            SDKManager.init(appInit.appContext, new SDKManagerCallback() {
+                @Override
+                public void onFinish() {
+                    LogUtil.e("init SDK success(threadname: " + Thread.currentThread().getName() + ")");
+                }
+            });
+            PrintManager.init(getApplicationContext());
+        } else {
+            Printooth.INSTANCE.init(this);
+        }
+
     }
 
 }
